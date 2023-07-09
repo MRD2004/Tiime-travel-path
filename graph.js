d77470df7b07da2abe7a8081e88792f55009af77
Manisha Rathod <95614461+MRD2004@users.noreply.github.com>
2023-07-09 07:16:16 +0530

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,14 @@
+function createGraph(V, E) {
+  // V - Number of vertices in graph
+  // E - Number of edges in graph (u,v,w)
+  let adj_list = []; // Adjacency list
+  for (let i = 0; i < V; i++) {
+    adj_list.push([]);
+  }
+  for (let i = 0; i < E.length; i++) {
+    adj_list[E[i][0]].push([E[i][1], E[i][2]]);
+    adj_list[E[i][1]].push([E[i][0], E[i][2]]);
+  }
+  return adj_list;
+}
+
